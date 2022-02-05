d2a8e79136f509f34e4a885aa53fcc46175778b8
RyanKruger1 <ryankruger45@gmail.com>
2022-02-05 20:15:45 +0200

    update database settings

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const database = new Sequelize('crypto', 'root' ,'goku',{
+const database = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_ROOT_USERNAME ,process.env.DATABASE_ROOT_PASSWORD,{
     dialect: 'mysql',
     host: 'localhost',
     port: '3306'
